42cfca902de2af8f4bf007323ad4db5e3d882d46
Matthew Hammond <mj.hammond@hotmail.co.uk>
2013-09-03 15:00:00 +0100

    Added jade and stylus templating

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	nib = require('nib');
+
 var app = express();
+
+function compile (str, path) {
+	return stylus (str)
+		.set('filename', path)
+		.use(nib())
+};
 
 var quotes = [
 	{ author : 'Marcel Proust', text : "The time which we have at our disposal every day is elastic; the passions we feel expand it, those that we inspire contract it, and habit fills up what remains.", id: 0 },
 	{ author : 'Aristotle', text : "We live in deeds, not years; in thoughts not breaths; in feelings, not in figures on a dial. We should count time by heart throbs. He most lives who thinks most, feels the noblest, acts the best.", id: 1 },
   	{ author : 'Muhammad Ali', text : "The fight is won or lost far away from the witnesses, behind the lines, in the gym, and out there on the road; long before I dance under those lights.", id: 2 },
-  	{ author : 'Leonardo', text : "Once you have flown, you will walk the earth with your eyes turned skywards, for there you have been, and there you long to return.", id: 3 }
+  	{ author : 'Leonardo', text : "Once you have flown, you will walk the earth with your eyes turned skywards, for there you have been, and there you long to return.", id: 3 },
+  	{ author : "Shakespeare", text : "And since you know you cannot see yourself, so well as by reflection, I, your glass, will modestly discover to yourself, that of yourself which you yet know not of.", id: 4 },
+	{ author : "Friedrich Nietzsche", text : "Many people wait throughout their whole lives for the chance to be good in their own fashion.", id: 5 }
 ];
 
 app.configure(function(){
@@ -15,13 +26,15 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
+app.use(stylus.middleware(
+	{ src: __dirname + '/public', compile: compile}));
 });
 
-app.get('/', function (req, res) {
+app.get('/quote/all', function (req, res) {
 	res.render('quotes', {title: 'Quotes', quotes: quotes});
 });
 
-app.get('/quote/random', function (req, res) {
+app.get('/', function (req, res) {
 	var id = Math.floor(Math.random() * quotes.length);
 	var q = quotes[id];
 	res.render('random', { q: q } );
