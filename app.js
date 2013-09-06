f2404bf263750450ed8ce33d4608df3175a4c0ab
Matthew Hammond <mj.hammond@hotmail.co.uk>
2013-09-06 19:42:01 +0100

    Change mongoose connection

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ var express = require('express'),
 	nib = require('nib'),
 	mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/quotes')
+var uristring = 
+process.env.MONGOLAB_URI || 
+process.env.MONGOHQ_URL || 
+'mongodb://localhost/quotes'
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err)
+  } else {
+  console.log ('Succeeded connecting to: ' + uristring)
+  }
+})
 
 var Schema = mongoose.Schema
 var QuoteSchema = new Schema({
