2f37e7f4e733f84649ad5e34c6485490c9a797d8
Matthew Hammond <mj.hammond@hotmail.co.uk>
2013-09-07 14:25:03 +0100

    add localhost mongo url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express'),
 var uristring = 
 process.env.MONGOLAB_URI || 
 process.env.MONGOHQ_URL || 
-'mongodb://heroku_app17969369:f0ffs6s67i4uqkm1ha3p86862v@ds043338.mongolab.com:43338/heroku_app17969369'
+'mongodb://localhost/quotes'
 
 mongoose.connect(uristring, function (err, res) {
   if (err) { 
